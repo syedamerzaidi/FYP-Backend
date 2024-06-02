d6284a129b19a0a28bb4302e39eb9d87fb26f2e1
Syed Muhammad Amer Ali Zaidi <amir.ali2036a@gmail.com>
2024-06-02 18:29:03 +0500

    fix build issue

--- src/main/java/com/fyp/hca/repositories/PatientRepository.java
+++ src/main/java/com/fyp/hca/repositories/PatientRepository.java
@@ -1,14 +1,10 @@
 package com.fyp.hca.repositories;
 
 import com.fyp.hca.entity.*;
-import com.fyp.hca.model.StatisticResponseModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
-import java.util.List;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
